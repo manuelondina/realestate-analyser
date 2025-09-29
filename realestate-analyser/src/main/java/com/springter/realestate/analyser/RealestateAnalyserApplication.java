39c4f73d28006ef12e294d6256e211f0d3c7933d
Manuel Ondina <43965936+manuelondina@users.noreply.github.com>
2025-09-29 14:27:42 +0200

    init: bootstrap Spring Boot project for real estate analyzer 🏠📋

---
+++ realestate-analyser/src/main/java/com/springter/realestate/analyser/RealestateAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.springter.realestate.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateAnalyserApplication.class, args);
+	}
+
+}
---
+++ realestate-analyser/src/test/java/com/springter/realestate/analyser/RealestateAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springter.realestate.analyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
